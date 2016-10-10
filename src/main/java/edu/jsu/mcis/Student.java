b270b52aaa379dfd4b844d2634cab69f0c0fb749
Jacob O'Dell <Jacob O'Dell>
2016-10-10 11:28:20 -0500

    adds student

---
+++ src/main/java/edu/jsu/mcis/Student.java
@@ -0,0 +1,63 @@
+package edu.jsu.mcis;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Student{
+	private String id;
+	private String firstName;
+	private String lastName; 
+	private String email; 
+
+    public Student() {
+        id = "";
+        firstName = "";
+        lastName = "";
+        email = "";
+    }
+    
+    public Student(String id) {
+        this.id = id;
+        firstName = "";
+        lastName = "";
+        email = "";
+    }
+    
+    @Override
+    public String toString() {
+        return "[" + getID() + "]" + getFirstName() + getLastName() + getEmail() + "@jsu.edu";
+    }
+    
+	public String getID(){
+		return this.id; 
+	}
+
+	public void setID(String id){
+		this.id = id; 
+	}
+
+	public String getFirstName(){
+		return this.firstName; 
+	}
+
+	public void setFirstName(String firstName){
+		this.firstName = firstName; 
+	}
+
+	public String getLastName(){
+		return this.lastName; 
+	}
+
+	public void setLastName(String lastName){
+		this.lastName = lastName; 
+	}
+
+	public String getEmail(){
+		return this.email; 
+	}
+
+	public void setEmail(String email){
+		this.email = email; 
+	}
+}
