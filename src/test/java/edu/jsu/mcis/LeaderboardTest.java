b3bab6595a1df3afbc2d1d1d929791d8b5a7d0da
Jacob O'Dell <odell486@gmail.com>
2016-10-11 09:45:40 -0500

    More Tests

--- src/test/java/edu/jsu/mcis/LeaderboardTest.java
+++ src/test/java/edu/jsu/mcis/LeaderboardTest.java
@@ -7,36 +7,16 @@ import java.io.*;
 import java.util.*; 
 
 public class LeaderboardTest{
-	private String csvStudentString;
-	private String csvCourseString;
-	private String line;  
-	private DataReader data; 
-	private Student student;
-	private Course course; 	
 
 	@Before
 	public void setUp(){
-		data = new DataReader();
+		reader = new DataReader();
 		student = new Student();
-	}
-
-	@Test
-	public void testReadsInStudentIDCorrectly(){
-		assertEquals(contains()"111111", data.readFile()); 
-	}
-
-	@Test
-	public void testSetGetID() {
-		Student s = new Student();
-		s.setID("123232");
-		assertEquals("123232", s.getID());
-	}
-
-
+    }
+    
 	@Test
 	public void testGetStudentList() {
-		DataReader r = new DataReader();
-		List<Student> stulist = r.readFile("student");
+		List<Student> stulist = reader.readFile("student");
 		assertEquals("111111", stulist.get(0).getID());
 	}
 
