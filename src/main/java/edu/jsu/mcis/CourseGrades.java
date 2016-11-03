6a65984d450a0b33420dae05b27ec79fe503f4c4
Matthew Willis <Matthew Willis>
2016-11-03 18:19:33 -0500

    Starting the coursegrades test

---
+++ src/main/java/edu/jsu/mcis/CourseGrades.java
@@ -0,0 +1,11 @@
+package edu.jsu.mcis;
+
+
+public class CourseGrades{
+	private String[] Assignment;
+	private String[] Exam;
+	private String grades;
+	
+	
+	
+	}
---
+++ src/test/java/edu/jsu/mcis/CourseGradesTest.java
@@ -0,0 +1,21 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.io.*;
+import java.util.*; 
+
+public class CourseGradesTest{
+	
+	private CourseGrades courseGrades;
+	
+	@Before
+	public void setUp(){
+		courseGrades = new CourseGrades();
+    }
+	
+	/*@Test
+	public void testTheFirstLine(){
+		courseGrades c = courseGrades.setId("111318");
+		assertEquals("111318",c.getId());
+	}*/
+}
